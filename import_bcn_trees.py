1525c596c61c18e17a346057b9d62053b20f6544
Carlos Cámara <ccamara@pm.me>
2019-12-20 23:05:32 +0100

    WIP in import_bcn_trees

--- import_bcn_trees.py
+++ import_bcn_trees.py
@@ -17,4 +17,25 @@ wget.download(url, 'data/raw/arbrat_viari.csv')
 
 bcn_arbrat_viari_raw = pd.read_csv('data/raw/arbrat_viari.csv')
 
-bcn_trees_df = bcn_trees.data_munging(bcn_arbrat_viari_raw)
+bcn_arbrat_viari = bcn_trees.data_munging(bcn_arbrat_viari_raw, 'arbrat_viari')
+
+
+print(bcn_arbrat_viari.dtypes)
+print(bcn_arbrat_viari['species:ca'].unique())
+
+
+# %% Arbrat zones
+
+url = "https://opendata-ajuntament.barcelona.cat/data/dataset/9b525e1d-13b8-48f1-abf6-f5cd03baa1dd/resource/8f2402dd-72dc-4b07-8145-e3f75004b0de/download"
+wget.download(url, 'data/raw/arbrat_zona.csv')
+
+bcn_arbrat_zona_raw = pd.read_csv('data/raw/arbrat_zona.csv')
+
+bcn_arbrat_zona = bcn_trees.data_munging(bcn_arbrat_zona_raw, 'arbrat_zona')
+
+# Combine dataframes into a single one
+bcn_trees_df = pd.concat([bcn_arbrat_viari, bcn_arbrat_zona])
+
+
+print(bcn_trees_df.dtypes)
+print(bcn_trees_df['species:ca'].unique())
