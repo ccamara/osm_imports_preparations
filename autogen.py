47c9621381d654869bc0cb4acd4a513c750c0ad8
Carlos Cámara <ccamara@pm.me>
2019-12-21 12:39:56 +0100

    Deployed b712c29 with MkDocs version: 1.0.4

---
+++ autogen.py
@@ -0,0 +1,16 @@
+# content of docs/autogen.py
+
+from keras_autodoc import DocumentationGenerator
+
+from src.features import bcn_trees
+
+
+
+pages = {
+    'api_bcn_trees.md': [
+        'bcn_trees.data_download',
+        'bcn_trees.data_munging']
+    }
+
+doc_generator = DocumentationGenerator(pages)
+doc_generator.generate('./sources')
