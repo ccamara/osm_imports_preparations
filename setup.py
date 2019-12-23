dbbd7d372da0d691cea9282ca63cdad2bbca931c
Carlos Cámara <ccamara@pm.me>
2019-12-23 11:32:50 +0100

    Adds setup.py file.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Scripts for preparing data improrts to OSM.',
+    author='Carlos Cámara',
+    license='MIT',
+)
